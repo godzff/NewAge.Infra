42a6da5b54a2225f0b417f9e245b900235a5fedd
godzff <godzff@gmail.com>
2020-05-02 11:02:11 +0800

    增加xml序列号

---
+++ src/Helpers/XmlHelper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewAge.Infra.Helpers
+{
+    /// <summary>
+    /// Xml操作
+    /// </summary>
+    public static class XmlHelper
+    {
+        /// <summary>
+        /// 将对象序列化为XML字符串
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="obj">对象</param>
+        /// <returns></returns>
+        public static string ToXml<T>(this T obj)
+        {
+            var jsonStr = obj.ToJson();
+            var xmlDoc = JsonConvert.DeserializeXmlNode(jsonStr);
+            string xmlDocStr = xmlDoc.InnerXml;
+
+            return xmlDocStr;
+        }
+
+        /// <summary>
+        /// 将对象序列化为XML字符串
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="obj">对象</param>
+        /// <param name="rootNodeName">根节点名(建议设为xml)</param>
+        /// <returns></returns>
+        public static string ToXml<T>(this T obj, string rootNodeName)
+        {
+            var jsonStr = obj.ToJson();
+            var xmlDoc = JsonConvert.DeserializeXmlNode(jsonStr, rootNodeName);
+            string xmlDocStr = xmlDoc.InnerXml;
+
+            return xmlDocStr;
+        }
+    }
+}
