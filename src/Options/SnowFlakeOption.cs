b963a42f1cf437ab6d98e06fee0650759faebbff
godzff <godzff@gmail.com>
2020-05-02 10:03:03 +0800

    增加雪花算法help

--- src/Options/SnowFlakeOption.cs
+++ src/Options/SnowFlakeOption.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NewAge.Infra
+namespace NewAge.Infra.Options
 {
     /// <summary>
     /// 雪花算法的参数配置
@@ -10,7 +10,7 @@ namespace NewAge.Infra
     public class SnowFlakeOption
     {
         /// <summary>
-        /// 工作Id 只能0-1022
+        /// 工作Id 只能1-1022
         /// </summary>
         public int WorkId { get; set; }
     }
