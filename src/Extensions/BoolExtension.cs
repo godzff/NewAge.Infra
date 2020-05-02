c2e256927c3cdc777bc6df4b1d7814d073680035
godzff <godzff@gmail.com>
2020-05-02 11:58:04 +0800

    增加bool,datetime扩展

---
+++ src/Extensions/BoolExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace NewAge.Infra.Extensions
+{
+    public static class BooltExtension
+    {
+        /// <summary>
+        /// 转Bool
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="defaultVal"></param>
+        /// <returns></returns>
+        public static bool ToBool(this object obj, bool defaultVal = false)
+        {
+            if (obj == null)
+                return defaultVal;
+            if (!bool.TryParse(obj.ToStr(), out bool retVal))
+                retVal = defaultVal;
+            return retVal;
+        }
+    }
+}
---
+++ src/Extensions/DateTimeExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NewAge.Infra.Extensions
+{
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// 转DateTime
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="defaultVal"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this object obj, DateTime defaultVal = new DateTime())
+        {
+            if (obj == null)
+                return defaultVal;
+            if (!DateTime.TryParse(obj.ToStr(), out DateTime retVal))
+                retVal = defaultVal;
+            return retVal;
+        }
+    }
+}
